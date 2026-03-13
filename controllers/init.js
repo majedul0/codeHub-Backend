f838e210fb1ef30c67957e49c61ea7725bc49808
majedul0 <islam22205101253@diu.edu.bd>
2026-03-13 14:11:49 +0600

    Initialize repository structure and create index.json file

--- controllers/init.js
+++ controllers/init.js
@@ -1,5 +1,21 @@
+import fs from 'fs/promises';
+
+
+import path from 'path';
+
+
 export async function initrepo() {
-    console.log("Initializing a new Repository...");
+    const repoPath = path.resolve(process.cwd(), '.mygit');
+    const commitespath=path.join(repoPath,'commits');
+
+    try {
+        await fs.mkdir(repoPath, { recursive: true });
+        await fs.mkdir(commitespath, { recursive: true });
+        await fs.writeFile(path.join(repoPath, 'index.json'), JSON.stringify({}), 'utf-8');
+        console.log('Repository initialized successfully.');
+    } catch (error) {
+        console.error('Error initializing repository:', error);
+    }
 }
 
 export default initrepo;
